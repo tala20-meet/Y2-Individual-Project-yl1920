3c563721e028271b716734592d8d0df4d90f458e
myname21-meet <myname21@meet.mit.edu>
2020-01-30 10:30:56 -0500

    a

--- database.py
+++ database.py
@@ -88,46 +88,27 @@ def get_all():
 	return movies
 
 
-'''
-add_movie("Glass","Drama", 2019 ,"static/img/glass.jpg") 
-add_movie("Harry Potter & the Philosopher's Stone" , "Fantasy" , 2001 ,"static/img/harry potter 1.jpg")
-add_movie("Harry Potter & the Chamber of Secrets" , "Fantasy" , 2002 ,"static/img/harry potter 2.png")
-add_movie("Harry Potter & the Prisoner of Azkaban" , " Fantasy" , 2004 , "static/img/harry potter 3.jpeg")
-add_movie("Harry Potter & the Goblet of Fire", "Fantasy" , 2005,"static/img/harry potter 4.jpg" )
-add_movie("The Avengers", "Sci-Fi", 2012 , "static/img/avengers 1.jpg")
-add_movie("Frozen 2","Animation",2019, "static/img/frozen2.jpg")
-add_movie("Frozen" , "Animation", 2013 ,"static/img/frozen1.jpeg")
-add_movie("Doctor sleep", "Horror", 2019 , "static/img/doctorsleep.jpg")
-add_movie("The Guilty", "Crime" , 2018, "static/img/theguilty.jpg")
-add_movie("Mission: Impossible - Fallout", "Action", 2018, "static/img/mission-impossible-fallout.jpg")
-add_movie("A Star IS Born", "Romance" , 2018, "static/img/a star is born.jpg")
-add_movie(" Blindspotting " , "Comedy", 2018, "static/img/blindspotting.jpg")
-add_movie("Avengers: Age of Ultron","Action", 2015,"static/img/avengers2.jpeg")
-add_movie(" Avengers: Infinity War ", "Action", 2018,"static/img/avengers3.jpg")
-add_movie("Deadpool 2" , "Action", 2018 ,"static/img/deadpool2.jpg")
-add_movie("Overlord" , "Action" , 2018 ,"static/img/overload.jpg")
-add_movie("Revenge", "Action" , 2018 ,"static/img/revenge.jpg")
-add_movie(" Mowgli: Legend of the Jungle" , "Adventure", 2018 ,"static/img/mowglie.jpg")
-add_movie("Monsters and Men " , " crime" , 2018, "static/img/monsters&men.jpg")
-add_movie("Black Panther" , " Action" , 2018 ,"static/img/blackpanther.jpeg")
-add_movie(" Get Out" , "Horror" , 2017 ," static/img/Get_Out_poster.png")
-add_movie(" Jumanji: Welcome to the Jungle", "Action ", 2017 ,"static/img/jumanji.jpg")'''
 
-
-'''
-add_movie()
-add_movie()
-add_movie()
-add_movie()
-add_movie()
-add_movie()
-add_movie()
-add_movie()
-
-add_movie()
-
-'''
-
-
-
-
+# add_movie("Glass","Drama", 2019 ,"static/img/glass.jpg") 
+# add_movie("Harry Potter & the Philosopher's Stone" , "Fantasy" , 2001 ,"static/img/harry potter 1.jpg")
+# add_movie("Harry Potter & the Chamber of Secrets" , "Fantasy" , 2002 ,"static/img/harry potter 2.png")
+# add_movie("Harry Potter & the Prisoner of Azkaban" , "Fantasy" , 2004 , "static/img/harry potter 3.jpeg")
+# add_movie("Harry Potter & the Goblet of Fire", "Fantasy" , 2005,"static/img/harry potter 4.jpg" )
+# add_movie("The Avengers", "Sci-Fi", 2012 , "static/img/avengers 1.jpg")
+# add_movie("Frozen 2","Animation",2019, "static/img/frozen2.jpg")
+# add_movie("Frozen" , "Animation", 2013 ,"static/img/frozen1.jpeg")
+# add_movie("Doctor sleep", "Horror", 2019 , "static/img/doctorsleep.jpg")
+# add_movie("The Guilty", "Crime" , 2018, "static/img/theguilty.jpg")
+# add_movie("Mission: Impossible - Fallout", "Action", 2018, "static/img/mission-impossible-fallout.jpg")
+# add_movie("A Star IS Born", "Romance" , 2018, "static/img/a star is born.jpg")
+# add_movie(" Blindspotting " , "Comedy", 2018, "static/img/blindspotting.jpg")
+# add_movie("Avengers: Age of Ultron","Action", 2015,"static/img/avengers2.jpeg")
+# add_movie(" Avengers: Infinity War ", "Action", 2018,"static/img/avengers3.jpg")
+# add_movie("Deadpool 2" , "Action", 2018 ,"static/img/deadpool2.jpg")
+# add_movie("Overlord" , "Action" , 2018 ,"static/img/overload.jpg")
+# add_movie("Revenge", "Action" , 2018 ,"static/img/revenge.jpg")
+# add_movie(" Mowgli: Legend of the Jungle" , "Adventure", 2018 ,"static/img/mowglie.jpg")
+# add_movie("Monsters and Men " , "Crime" , 2018, "static/img/monsters&men.jpg")
+# add_movie("Black Panther" , "Action" , 2018 ,"static/img/blackpanther.jpeg")
+# add_movie(" Get Out" , "Horror" , 2017 ," static/img/Get_Out_poster.png")
+# add_movie(" Jumanji: Welcome to the Jungle", "Action ", 2017 ,"static/img/jumanji.jpg")
